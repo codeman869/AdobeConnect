eb8946141e501d2488b58401f67385ee82eb2a27
Cody Deckard <cjdeckard@gmail.com>
2018-08-27 11:44:31 -0700

    Adds a MeetingList Collection
    Adds an observable collection MeetingList as a collection of
    meetings which is provided as the data source for the main
    window. Also removes some unnecessary headers.

---
+++ Adobe Connect/Collections/MeetingList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.ObjectModel;
+using Adobe_Connect.Models;
+
+namespace Adobe_Connect.Collections
+{
+    class MeetingList : ObservableCollection<Meeting>
+    {
+    }
+}
--- Adobe Connect/MainWindow.xaml.cs
+++ Adobe Connect/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Adobe_Connect.Models;
+using Adobe_Connect.Services;
+using Adobe_Connect.Collections;
 
 namespace Adobe_Connect
 {
@@ -21,8 +12,8 @@ namespace Adobe_Connect
     public partial class MainWindow : Window
     {
 
-        private List<Models.Meeting> meetings;
         
+        private MeetingList meetings;
 
         public MainWindow()
         {
@@ -40,19 +31,23 @@ namespace Adobe_Connect
         private async void GetMeetingsButton_Click(object sender, RoutedEventArgs e)
         {
 
-            System.Windows.Data.CollectionViewSource meetingViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("meetingViewSource")));
-            meetings = await Services.AdobeConnectService.GetMeetings();
+            List<Meeting> tempList = await AdobeConnectService.GetMeetings();
 
-            meetingViewSource.Source = meetings;
-
+            foreach(Meeting meeting in tempList)
+            {
+                meetings.Add(meeting);
+            }
+            
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            //System.Windows.Data.CollectionViewSource meetingViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("meetingViewSource")));
-            //System.Windows.Data.CollectionViewSource meetingViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("meetingViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            //meetingViewSource.Source = meetings;
+            
+            System.Windows.Data.CollectionViewSource meetingViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("meetingViewSource")));
+            
+            meetings = new MeetingList();
+
+            meetingViewSource.Source = meetings;
         }
     }
 }
